da6bc4e7773b998244be832e1010b4b456b18c81
Oluwasemilore Kayode-Yusuf <okayodey@uoguelph.ca>
2025-09-29 12:32:29 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -3,5 +3,19 @@
 # Fibonacci Sequence Exercise
 # TODO: (Read detailed instructions in the Readme file)
 # Prompt the user for the number of terms.
+for i in range(7):  
+    user_inp = int(input("User input: "))
 # Validate that the input is a positive integer.
-# Use a for loop to print the Fibonacci sequence up to that many terms.
+  if user_inp > 0:
+        break
+    else:
+        print("Expected output: Please enter a positive integer.")
+else:
+    print("Too many invalid attempts. Exiting...")
+# Use a for loop to print the Fibonacci sequence up to that many terms
+a,b=0,1
+print("Expected output:", end=" ")
+for i in range(user_inp):
+    print(a, end=" ")
+    a, b = b, a + b
+print()
